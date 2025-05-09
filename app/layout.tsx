48af4eacc5782075be2e10673a29f0745207e401
rusty <rust.starman@gmail.com>
2025-05-09 22:28:08 +0300

    Update @vercel/analytics to version 1.5.0 and integrate Analytics component in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Toaster } from 'sonner';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from '@vercel/analytics/next';
 
 import './globals.css';
 import { SessionProvider } from 'next-auth/react';
@@ -79,6 +80,7 @@ export default async function RootLayout({
         >
           <Toaster position="top-center" />
           <SessionProvider>{children}</SessionProvider>
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
